be54fb45dd4e1fe7a3f1adf5ce4b9eb5a26441d6
FalaleevDanila <47750074+FalaleevDanila@users.noreply.github.com>
2020-01-27 22:25:42 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,19 +1,11 @@
 #include "include/widget.h"
 #include "include/mainwindow.h"
 #include <QApplication>
-#include "test_class.h"
-
-#include <QTest>
 
 int main(int argc, char *argv[])
 {
 
-    //======
-    //freopen("testing.log", "w", stdout);
     QApplication a(argc, argv);
-    //QTest::qExec(new Test_class, argc, argv);
-    //======
-    //QApplication a(argc, argv);
     MainWindow w;
 
     w.show();
